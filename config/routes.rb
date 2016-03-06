84bccf46eed9f58bdc511316f414491889cb14d1
xjtlucsse <xjtlu.csse@outlook.com>
2016-03-06 14:35:21 +0800

    "test 2"

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Proj1::Application.routes.draw do
-  root to: 'static_pages#home'
+
   match '/home', to: 'static_pages#home'
   match '/about', to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
